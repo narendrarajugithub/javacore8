4025a214184fe66cecdf463395e25936efe888ab
narendrarajugithub <ynarendraraju@gmail.com>
2020-02-16 23:44:16 +0530

    commit

--- src/com/ust/core/java8/foreach/TestForeach.java
+++ src/com/ust/core/java8/foreach/TestForeach.java
@@ -1,8 +1,7 @@
 package com.ust.core.java8.foreach;
 
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
